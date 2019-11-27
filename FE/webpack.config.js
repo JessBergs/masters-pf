76d324524013e552aeb18958dcfe4842de30fc4d
JessBergs <info@jbergs.eu>
2019-11-27 19:23:20 +0000

    Set up webpack / babel, remove react-scripts

--- FE/webpack.config.js
+++ FE/webpack.config.js
@@ -1,43 +1,25 @@
-const path = require('path');
-const nodeExternals = require('webpack-node-externals');
-const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
-
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
-  entry: './src/index.js', // relative path
-  output: {
-    path: path.join(__dirname, 'dist'), // absolute path
-    filename: 'main.js', // file name
-    libraryTarget: 'commonjs',
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    port: 9000
-  },
-  externals: [nodeExternals()],
-  target: 'node',
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        type: 'javascript/auto',
+        use: {
+          loader: "babel-loader"
+        }
       },
       {
-        test: /\.s[ac]ss$/i,
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+    },
+      {
+        test: /\.html$/,
         use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.mjs$/,
-        include: /node_modules/,
-        type: 'javascript/auto',
+          {
+            loader: "html-loader"
+          }
+        ]
       },
       {
         test: /\.(woff(2)?|ttf|eot|svg|png)(\?v=\d+\.\d+\.\d+)?$/,
@@ -55,9 +37,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-       template: "./public/index.html",
-       filename: 'index.html'
+      template: "./public/index.html",
+      filename: "./index.html"
     })
- ]
+  ]
 };
-
