50ed81d91d153ffade3593fbc2fc47e0996eccc8
JessBergs <info@jbergs.eu>
2019-11-28 00:13:28 +0000

    Tweak index page

--- FE/src/components/IndexPage.jsx
+++ FE/src/components/IndexPage.jsx
@@ -1,50 +1,62 @@
 import React from 'react';
 
 import '../styles/indexPage.scss';
-import {projects} from '../../data/projects.json';
+import { projects } from '../../data/projects.json';
 
 const Header = () => (
     <div className="index-page__header-container">
-        <div className="index-page__header-video">
-            <img src="https://jbergs.eu/img_index/titlemov_poster.jpg" />
-        </div>
+        <video
+            autoPlay
+            muted
+            loop
+            className="index-page__header-video"
+            poster="https://jbergs.eu/img_index/titlemov_poster.jpg"
+        >
+            <source src="https://jbergs.eu/img_index/titlemov.mp4" type="video/mp4" />
+            <source src="https://jbergs.eu/img_index/titlemov.webm" type="video/webm" />
+            <source src="https://jbergs.eu/img_index/titlemov.ogv" type="video/ogg" />
+        </video>
+        <img className="index-page__header-video--hidden" src="https://jbergs.eu/img_index/titlemov_poster.jpg" />
+
         <div className="index-page__header-text">
             <div className="index-page__header-title">Jessica Bergs</div>
             <div className="index-page__header-subtitle">Master's portfolio</div>
             <div className="index-page__header-subline">
                 M.Sc. Visualisation: Serious Games Development
                 <br />
-                The Glasgow School of Art{' '}
+                The Glasgow School of Art
             </div>
         </div>
     </div>
 );
 
-const ProjectItem = ({ projectTitle, subline, linkUrl }) =>     {
-    return(
-    <div className="index-page__project-item">
-        <a className="index-page__project-link" href={linkUrl}>
-            <div className="index-page__project-image">
-                <img
-                    className="index-page__project-image--hidden"
-                    src="https://jbergs.eu/img_index/indexer_lab2_square.jpg"
-                />
-            </div>
-            <div className="index-page__project-text-overlay">
-                <div className="index-page__project-title">{projectTitle}</div>
-                <div className="index-page__project-sub">{subline}</div>
-            </div>
-        </a>
-    </div>
-)};
+const ProjectItem = ({ projectTitle, subline, linkUrl }) => {
+    return (
+        <div className="index-page__project-item">
+            <a className="index-page__project-link" href={linkUrl}>
+                <div className="index-page__project-image">
+                    <img
+                        className="index-page__project-image--hidden"
+                        src="https://jbergs.eu/img_index/indexer_lab2_square.jpg"
+                    />
+                </div>
+                <div className="index-page__project-text-overlay">
+                    <div className="index-page__project-title">{projectTitle}</div>
+                    <div className="index-page__project-sub">{subline}</div>
+                </div>
+            </a>
+        </div>
+    );
+};
 
-const ProjectCollection = ({content}) => (
+const ProjectCollection = ({ content }) => (
     <div className="index-page__layout-container">
         <div className="index-page__heading">Key projects</div>
         <div className="index-page__projects-container">
             <div className="index-page__projects-grid">
-                {content.map((item) => (
-                       <ProjectItem {...item}/>))}
+                {content.map(item => (
+                    <ProjectItem {...item} />
+                ))}
             </div>
         </div>
     </div>
@@ -69,16 +81,11 @@ const Footer = () => (
                 text="Spatial virtaces, 3D reconstruction, 3D modelling, IoT wearables, heal"
             />
             <FooterItem
-                text="
-© 2018 Jessica Bergs
-All rights reserved
-"
+                text="© 2018 Jessica Bergs All rights reserved"
             />
         </div>
     </div>
 );
-
-
 
 const IndexPage = () => (
     <div className="index-page__container">
@@ -86,7 +93,7 @@ const IndexPage = () => (
             <Header />
         </section>
         <section className="index-page__projects-section">
-            <ProjectCollection content={projects}/>
+            <ProjectCollection content={projects} />
         </section>
         <section className="index-page__footer-section">
             <Footer />
---
+++ FE/src/components/ProjectPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import '../styles/indexPage.scss';
+import { projects } from '../../data/projects.json';
+
+const ProjectPage = () => (
+    <div className="project-page__container">
+
+    </div>
+);
+
+export default ProjectPage;
