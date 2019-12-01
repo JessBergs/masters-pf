d37efa62846556e61a2757a90bb45080936645a4
JessBergs <info@jbergs.eu>
2019-12-01 17:19:07 +0000

    Updates

--- FE/__mocks__/axios.js
+++
@@ -1,7 +0,0 @@
-const axios = {
-    get: jest.fn(() => Promise.resolve({ data:[{test: 'test'}] })),
-    post: jest.fn(() => Promise.resolve({ data:[{test: 'test'}] })),
-    delete: jest.fn(() => Promise.resolve({ data:[{test: 'test'}] })),
-  };
-  
-module.exports = axios;
--- FE/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Routing from './Routing.jsx';
-import * as serviceWorker from './serviceWorker.js';
-
-ReactDOM.render(<Routing />, document.getElementById('root'));
-
-serviceWorker.unregister();
