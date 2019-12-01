b970e28adcdc6530438fd26eb15d1e1ce3f308cc
JessBergs <info@jbergs.eu>
2019-12-01 00:25:10 +0000

    Add Lambda API

--- BE/handler.js
+++ BE/handler.js
@@ -1,8 +1,23 @@
+var AWS = require('aws-sdk');
+
+const docClient = new AWS.DynamoDB.DocumentClient({region: 'eu-central-1'});
+
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from J!'),
-    };
-    return response;
+
+    const params = {
+        TableName: "masters-pf"
+      };
+
+    return new Promise(function (resolve, reject) {
+        docClient.scan(params, function (err, data) {
+            if (err) {
+                console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+            } else {
+                resolve({
+                    statusCode: 200,
+                    body: JSON.stringify(data)
+                });
+            }
+        });
+    });
 };
